190e1fd986fa693d0634ce1e6bf20ee903d47d98
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-01-12 01:14:31 +0200

    Task ~done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import SectionTitle from "./Components/TitleSec";
+import Statistics from "./Components/Statistics";
+import FeedbackOptions from "./Components/FeedbackOptions";
+import Notification from "./Components/Notification";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onIncrementGood = () => {
+    this.setState((prevState) => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  onIncrementNeutral = () => {
+    this.setState((prevState) => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  onIncrementBad = () => {
+    this.setState((prevState) => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = ({ good, neutral, bad }) => {
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
+    const totalFeedback = good + neutral + bad;
+
+    let positFeedPerctenger = (good / totalFeedback) * 100;
+
+    return (positFeedPerctenger = positFeedPerctenger
+      ? positFeedPerctenger
+      : 0);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className="App">
+        <SectionTitle title={"Please leave feedback"}>
+          <FeedbackOptions
+            onIncrementGood={this.onIncrementGood}
+            onIncrementNeutral={this.onIncrementNeutral}
+            onIncrementBad={this.onIncrementBad}
+          />
+          {good || neutral || bad ? (
+            <div>
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={this.countTotalFeedback(this.state)}
+                positivePercentage={this.countPositiveFeedbackPercentage(
+                  this.state
+                )}
+              />{" "}
+            </div>
+          ) : (
+            <Notification message={"There is no feedback"} />
+          )}
+        </SectionTitle>
+      </div>
+    );
+  }
 }
 
 export default App;
